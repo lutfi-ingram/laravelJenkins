f2fe152f001b42cb5771781756039c219ebd3186
Yanuar Lutfi <lutpiero@gmail.com>
2022-02-23 11:47:16 +0700

    Fix template.json, add wait

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,8 +1,8 @@
-/* groovylint-disable-next-line CompileStatic */
+/* groovylint-disable-next-line CompileStatic 
 pipeline {
     agent any
     triggers {
-        pollSCM '*/1 * * * *'
+        pollSCM '1 * * * *'
     }
 
     stages {
@@ -46,6 +46,67 @@ pipeline {
             steps {
                 script {
                     openshift.withCluster( 'devocpcluster' ) {
+                        def dc = openshift.selector( "dc/${params.APPLICATION}" )
+                        try {
+                            def rollout = dc.rollout()
+                            def resultRollout = rollout.latest()
+                            resultRollout.logs('-f')
+                    } catch (Exception ex) {
+                            println(ex.getMessage())
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+*/
+pipeline {
+    agent any
+    triggers {
+        pollSCM '*/1 * * * *'
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm: [ 
+                    $class: 'GitSCM',
+                    userRemoteConfigs: [[url: 'https://github.com/lutfi-ingram/cakephp-ex.git']],
+                    branches: [[name: 'refs/heads/master']]
+                ], poll: true
+            }
+        }
+        stage('Create App') {
+            steps {
+                script {
+                    openshift.withCluster() {
+                        try {
+                            def created = openshift.newApp( 'templates.json' )
+                        } catch (Exception ex) {
+                            println(ex.getMessage())
+                        }
+                    }
+                }
+            }
+        }
+        stage('Build') {
+            steps {
+                script {
+                    openshift.withCluster() {
+                        def bc = openshift.selector( "bc/${params.BC_NAME}" )
+                        def result = bc.startBuild()
+                        timeout(10) {
+                            result.logs('-f')
+                        }
+                    }
+                }
+            }
+        }
+        stage('Rollout') {
+            steps {
+                script {
+                    openshift.withCluster( "${params.CLUSTER_NAME}" ) {
                         def dc = openshift.selector( "dc/${params.APPLICATION}" )
                         try {
                             def rollout = dc.rollout()
